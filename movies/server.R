b614bae0d63911fb9cb10d203c73ffa7699f4529
Marianna3011 <132451606+Marianna3011@users.noreply.github.com>
2024-06-06 19:54:23 +0200

    adedd about

--- movies/server.R
+++ movies/server.R
@@ -9,7 +9,8 @@ movies2 <- movies %>%
   select(Year, Rating, Metascore, Title, Director, Genre, `Duration (min)`)
 movies <- movies %>%
   select(Year, Rating, Title, Director, Genre)
-
+movies_separated <- movies %>%
+  separate_rows(Genre, sep = ",\\s*")
 
 top5_movies <- movies %>%
   group_by(Year) %>%
@@ -143,12 +144,66 @@ create_plot_Durations <- function() {
   return(violin_plot)
 }
 
+
+
+
+create_realeases_by_year <- function() {
+  # Aggregate data to count the number of movies per year for each genre
+  movies_per_year_genre <- movies_separated %>%
+    group_by(Year, Genre) %>%
+    summarise(Number_of_Movies = n()) %>%
+    ungroup()
+  
+  # Aggregate data to count the total number of movies per year
+  movies_per_year_all <- movies %>%
+    group_by(Year) %>%
+    summarise(Number_of_Movies = n()) %>%
+    mutate(Genre = "All Movies") %>%
+    ungroup()
+  
+  # Combine both data frames
+  movies_per_year_combined <- bind_rows(movies_per_year_genre, movies_per_year_all)
+  
+  # Create initial plot
+  initial_genre <- "All Movies"
+  line_graph <- plot_ly(data = movies_per_year_combined %>% filter(Genre == initial_genre),
+                        x = ~Year, y = ~Number_of_Movies, type = 'scatter', mode = 'lines+markers',
+                        name = initial_genre) %>%
+    layout(
+      title = "Number of Movies Released Each Year",
+      xaxis = list(title = "Year"),
+      yaxis = list(title = "Number of Movies"),
+      updatemenus = list(
+        list(
+          active = 0,
+          buttons = lapply(c("All Movies", unique(movies_separated$Genre)), function(genre) {
+            list(
+              args = list(list(y = list(movies_per_year_combined$Number_of_Movies[movies_per_year_combined$Genre == genre]),
+                               x = list(movies_per_year_combined$Year[movies_per_year_combined$Genre == genre])),
+                          list(title = paste("Number of Movies Released Each Year -", genre))),
+              label = genre,
+              method = "restyle"
+            )
+          }),
+          direction = "down",
+          x = 0.05,
+          xanchor = "left",
+          y = 1.15,
+          yanchor = "top"
+        )
+      )
+    )
+  return(line_graph)
+  
+}
+
 # Define server function
 shinyServer(function(input, output) {
   output$topMoviesPlot <- renderPlotly({
     create_plot(input$year)
   })
   output$Duration <-renderPlotly({create_plot_Durations()})
+  output$Releases <-renderPlotly({create_realeases_by_year()})
   output$menu <- renderMenu({
     sidebarMenu(
       menuItem("Menu item", icon = icon("calendar"))
--- movies/ui.R
+++ movies/ui.R
@@ -7,34 +7,47 @@ ui <- dashboardPage(
   dashboardHeader(title = img(src = "PP_logotyp_RGB.png", width=200, align = "right") ),
   dashboardSidebar(
     sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard"),
-                   menuItem("About", tabName = "about"))
+      menuItem("About", tabName = "about"),
+      menuItem("Dashboard", tabName = "dashboard"),
+      menuItem("Genres", tabName = "genres"))
   ),
   dashboardBody(  tabItems(
     tabItem(tabName = "dashboard",   
             fluidRow(
-      box(
-        title = "Select Year and View Top Movies",
-        numericInput("year", "Select Year:", value = 2024, min = 1929, max = 2025, step = 1),
-        plotlyOutput("topMoviesPlot")
-      ),
-      box(
-        title = "Distribution of Movie Durations by Genre",
-        plotlyOutput("Duration")
-      ),
-      width = 10
-    )
+              box(
+                title = "Select Year and View Top Movies",
+                numericInput("year", "Select Year:", value = 2024, min = 1929, max = 2025, step = 1),
+                plotlyOutput("topMoviesPlot")
+              ),
+              box(
+                title = "Distribution of Movie Durations by Genre",
+                plotlyOutput("Duration")
+              ),
+              box(
+                title = "Movie Releases by Year",
+                plotlyOutput("Releases")
+              ),
+              width = 10
+            )
             
     ),
     
     tabItem(tabName = "about",
-            h2("Widgets tab content")
+            h2("About This Dashboard"),
+            p("Welcome to our Movie Dashboard, your one-stop destination for exploring comprehensive data visualizations about movies! Dive into an array of insightful charts and graphs that showcase various aspects of the cinematic world."),
+            p("Key Features:"),
+            tags$ul(
+              tags$li("Movie Lengths: Discover trends and patterns in the lengths of movies across different genres and years. See how the duration of films has evolved over time."),
+              tags$li("Top Movies: Explore lists of the best movies from different years and genres. Our curated data highlights critically acclaimed and popular films that have left a mark on audiences and critics alike."),
+              tags$li("Genre Analysis: Gain insights into various movie genres. Compare and contrast their popularity, average ratings, and box office performance."),
+              tags$li("Yearly Trends: Understand how the film industry has changed over the years. Look at the number of movies released annually, their average ratings, and other key metrics."),
+              tags$li("Interactive Visualizations: Engage with interactive charts that allow you to filter and drill down into specific data points. Customize your view to focus on the information that interests you most.")
+            )
     )
   )
-  
- 
   )
 )
+
 
 # Export the UI
 shinyUI(ui)
